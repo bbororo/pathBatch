9b6c3aa943ac08a5c19e56af7f028b154aa91afe
dannab <koogoori54@gmail.com>
2024-04-10 18:37:37 +0900

    fisrt commit

---
+++ src/main/java/org/prac/pathbatch/PathBatchApplication.java
@@ -0,0 +1,13 @@
+package org.prac.pathbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PathBatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/prac/pathbatch/PathBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package org.prac.pathbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PathBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
